41587d17f3f4c9e199f102736bce3ecb74586e7a
punseee <sebastianogange@gmail.com>
2026-07-16 22:54:47 +0200

    Activate OOS evidence in Telegram notifications

--- project/synchronized_main.py
+++ project/synchronized_main.py
@@ -2,6 +2,11 @@
 from __future__ import annotations
 
 import project.main as modular_main
+import project.notification_engine.service as notification_service
+from project.notification_engine.robust_formatters import (
+    format_robust_signal_message,
+    format_robust_watchlist_message,
+)
 from project.research_engine.robust_memory import MemoryEfficientRobustResearchEngine
 from project.research_engine.robust_repository import RobustResearchRepository
 from project.synchronized_scheduler import SynchronizedPlatformScheduler
@@ -14,6 +19,8 @@ modular_main.PlatformScheduler = SynchronizedPlatformScheduler
 modular_main.ResearchRepository = RobustResearchRepository
 modular_main.MemoryEfficientResearchEngine = MemoryEfficientRobustResearchEngine
 modular_main.ProgressiveOperationalStrategyEngine = RobustEdgeOperationalStrategyEngine
+notification_service.format_signal_message = format_robust_signal_message
+notification_service.format_watchlist_message = format_robust_watchlist_message
 
 
 if __name__ == "__main__":
