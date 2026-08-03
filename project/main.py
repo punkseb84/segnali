0a5d8e6957271b91bf05e11bb077e6c8bea193f8
punseee <sebastianogange@gmail.com>
2026-08-03 20:14:29 +0200

    Route primary entrypoint to relative-strength V2

--- project/main.py
+++ project/main.py
@@ -1,7 +1,7 @@
-"""Primary Railway entrypoint for the relative-strength PAPER strategy."""
+"""Primary Railway entrypoint for the complete relative-strength V2 PAPER runtime."""
 from __future__ import annotations
 
-from project.relative_strength_main import main
+from project.relative_strength_runtime_v2 import main
 from project.simple_main import RAILWAY_LIGHT_MISSING_DATABASE_URL, build_postgres
 
 __all__ = [
